902eb023e6037bfbae8e5f33167734a5c4ee0e5b
unknown <firaoltulu5@gmail.com>
2023-08-08 22:37:52 +0300

    my ninth commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-"""This Program Print all alphabet except q and e in lowercase not new line at the end."""
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
