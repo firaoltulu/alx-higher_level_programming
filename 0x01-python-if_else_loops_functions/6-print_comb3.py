e32ed3ebea9910d3bc53f9a12bbd8b0a86262d07
unknown <firaoltulu5@gmail.com>
2023-08-08 22:35:48 +0300

    my ninth commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
-"""This program Print all possible different combinations of two digits in ascending order."""
+"""This program Print all possible different 
+    combinations of two digits in ascending order."""
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
         if digit1 == 8 and digit2 == 9:
