480839cb6e35d42a93934d6584ced5017d1c442c
unknown <firaoltulu5@gmail.com>
2023-08-08 22:33:13 +0300

    my eight commit

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -2,7 +2,4 @@
 
 def add(a, b):
     """This Function Return the addition of a and b."""
-    """@a: Is a number to be added to b."""
-    """@b: Is a number to be added to a."""
     return (a + b)
-
