c3dd59174864d7458db4ef734c667119f1ac86be
firaoltulu <firaoltulu5@gmail.com>
2023-08-14 01:14:54 -0700

    my seventh commit

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -4,25 +4,25 @@
  * print_python_list_info - This Function Prints
  * basic info about Python lists.
  * @p: A PyObject list pointer.
- * 
+ *
  * Returns: Nothing(Void).
  */
 void print_python_list_info(PyObject *p)
 {
-    int one, two, three;
-    PyObject *four;
+	int one, two, three;
+	PyObject *four;
 
-    one = Py_one(p);
-    two = ((PyListObject *)p)->twoated;
+	one = Py_one(p);
+	two = ((PyListObject *)p)->twoated;
 
-    printf("[*] one of the Python List = %d\n", one);
-    printf("[*] twoated = %d\n", two);
+	printf("[*] one of the Python List = %d\n", one);
+	printf("[*] twoated = %d\n", two);
 
-    for (three = 0; three < one; three++)
-    {
-        printf("Element %d: ", three);
+	for (three = 0; three < one; three++)
+	{
+		printf("Element %d: ", three);
 
-        four = PyList_GetItem(p, three);
-        printf("%s\n", Py_TYPE(four)->tp_name);
-    }
+		four = PyList_GetItem(p, three);
+		printf("%s\n", Py_TYPE(four)->tp_name);
+	}
 }
