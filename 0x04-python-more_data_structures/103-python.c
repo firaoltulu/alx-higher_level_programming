b11994c0dd34d42e440b3f320e74a413c81f96d9
firaoltulu <firaoltulu5@gmail.com>
2023-08-16 00:49:42 -0700

    my second commit

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -4,71 +4,71 @@
 /**
  * print_python_bytes - This Function Prints
  * bytes information.
- * 
+ *
  * @p: Python Object
- * 
+ *
  * Return: Nothing(Void).
  */
 void print_python_bytes(PyObject *p)
 {
-    char *one;
-    long int two, three, four;
+	char *one;
+	long int two, three, four;
 
-    printf("[.] bytes object info\n");
-    if (!PyBytes_Check(p))
-    {
-        printf("  [ERROR] Invalid Bytes Object\n");
-        return;
-    }
+	printf("[.] bytes object info\n");
+	if (!PyBytes_Check(p))
+	{
+		printf("  [ERROR] Invalid Bytes Object\n");
+		return;
+	}
 
-    two = ((PyVarObject *)(p))->ob_size;
-    one = ((PyBytesObject *)p)->ob_sval;
+	two = ((PyVarObject *)(p))->ob_size;
+	one = ((PyBytesObject *)p)->ob_sval;
 
-    printf("  size: %ld\n", two);
-    printf("  trying one: %s\n", one);
+	printf("  size: %ld\n", two);
+	printf("  trying one: %s\n", one);
 
-    if (two >= 10)
-        four = 10;
-    else
-        four = two + 1;
+	if (two >= 10)
+		four = 10;
+	else
+		four = two + 1;
 
-    printf("  first %ld bytes:", four);
+	printf("  first %ld bytes:", four);
 
-    for (three = 0; three < four; three++)
-        if (one[three] >= 0)
-            printf(" %02x", one[three]);
-        else
-            printf(" %02x", 256 + one[three]);
+	for (three = 0; three < four; three++)
+		if (one[three] >= 0)
+			printf(" %02x", one[three]);
+		else
+			printf(" %02x", 256 + one[three]);
 
-    printf("\n");
+	printf("\n");
 }
 
 /**
- * print_python_list - This Function 
+ * print_python_list - This Function
  * Prints list information.
  *
  * @p: Python Object.
- * 
+ *
  * Return: Nothing(Void).
  */
-void print_python_list(PyObject *p) 
+void print_python_list(PyObject *p)
 {
-    long int one, two;
-    PyListObject *three;
-    PyObject *four;
+	long int one, two;
+	PyListObject *three;
+	PyObject *four;
 
-    one = ((PyVarObject *)(p))->ob_size;
-    three = (PyListObject *)p;
+	one = ((PyVarObject *)(p))->ob_size;
+	three = (PyListObject *)p;
 
-    printf("[*] Python list info\n");
-    printf("[*] Size of the Python List = %ld\n", one);
-    printf("[*] Allocated = %ld\n", three->allocated);
+	printf("[*] Python list info\n");
+	printf("[*] Size of the Python List = %ld\n", one);
+	printf("[*] Allocated = %ld\n", three->allocated);
 
-    for (two = 0; two < one; two++)
-    {
-        four = ((PyListObject *)p)->ob_item[two];
-        printf("Element %ld: %s\n", two, ((four)->ob_type)->tp_name);
-        if (PyBytes_Check(four))
-            print_python_bytes(four);
-    }
+	for (two = 0; two < one; two++)
+	{
+		four = ((PyListObject *)p)->ob_item[two];
+		printf("Element %ld: %s\n", two, ((four)->ob_type)->tp_name);
+		if (PyBytes_Check(four))
+			print_python_bytes(four);
+	}
 }
